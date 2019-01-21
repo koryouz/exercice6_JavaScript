242bdb437528762506765acc7f89152e150387e9
DELHAY <delhayantoine@gmail.com>
2019-01-21 15:35:54 +0100

    Correction de l'équation de calcul dans le JS

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function toggle() {
   var nb1 = document.getElementById("firstNumber").value;
   var nb2 = document.getElementById("secondNumber").value;
-  var result = nb1/nb2;
+  var result = nb1%nb2;
   alert(result);
 }
